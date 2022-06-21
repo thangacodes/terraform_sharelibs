6123804bab8bf052a329d6c13f296abff503bd0e
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-06-21 22:46:49 +0530

    Update calculator.groovy
    function name changed according to the needs

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,12 +1,12 @@
 def add(x,y){
   echo "sum of ${x} and ${y} is ${x+y}"
 }
-def add(x,y){
+def multiply(x,y){
   echo "multiplication of ${x} and ${y} is ${x*y}"
 }
-def add(x,y){
+def substract(x,y){
   echo "Subtraction of ${x} and ${y} is ${x-y}"
 }
-def add(x,y){
+def divison(x,y){
   echo "Divison of ${x} and ${y} is ${x/y}"
 }
