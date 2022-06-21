0a5e51efa47126e71c316ceb857bd4f644c23ba0
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-06-21 22:43:02 +0530

    Create calculator.groovy
    initial commit

---
+++ vars/calculator.groovy
@@ -0,0 +1,12 @@
+def add(x,y){
+  echo "sum of ${x} and ${y} is ${x+y}"
+}
+def add(x,y){
+  echo "multiplication of ${x} and ${y} is ${x*y}"
+}
+def add(x,y){
+  echo "Subtraction of ${x} and ${y} is ${x-y}"
+}
+def add(x,y){
+  echo "Divison of ${x} and ${y} is ${x/y}"
+}
