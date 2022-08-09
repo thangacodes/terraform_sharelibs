5a7858d1ae031c3a95018f5e44ada6337dd247e2
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-08-09 14:31:28 +0530

    Update SourceCheckout.groovy

--- vars/SourceCheckout.groovy
+++ vars/SourceCheckout.groovy
@@ -1,8 +1,17 @@
-@Library('terraform_iac'@main') _
-node
-{
-   stage('GitCheckout')
-   {
-       sourceCheckout("https://github.com/send2durai/ansible-notes.git","main")
+def call (def giturl, def branch){
+        echo ""
+		echo ""********************** Git URL: ${giturl} ***********"
+		echo "hey am checking in"
+		echo "*********************** Git Branch: ${branch} *********"
+		echo "hey am still validating it"
+		if(giturl.contains("github.com"))
+		{
+		checkout([$class: 'GitSCM', branches: [[name: branch ]], extensions: [], userRemoteConfigs: [
+		                 [credentialsId: 'github_cred', url: giturl]
+						 ]])
+		}
+		else
+		{
+		    error("Please provide valid github url")
+		}
 	}
-}
