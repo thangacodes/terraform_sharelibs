fd4a9cf6a0868e317abd9bfe10bc4a7c2bc7e5da
Murugan <thangadurai.murugan@tesco.com>
2023-01-29 19:04:31 +0530

    initial commit

---
+++ jenkins/tf.groovy
@@ -0,0 +1,17 @@
+def tfinit(){
+    echo 'Terraform initialization in progress'
+}
+def tfformat(){
+    echo 'Terraform format in progress'
+}
+def tfvalidate(){
+    echo 'Terraform validate in progress'
+}
+def tfplan(){
+    echo 'Terraform plan in progress'
+}
+def tfapply(){
+    echo 'Terraform apply in progress'
+}
+
+return this
