c1236d5b619fdb696986e8f621f8096f3fd677f3
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2023-01-30 22:43:30 +0530

    Update variables.tf

--- ec2_spinup/variables.tf
+++ ec2_spinup/variables.tf
@@ -2,3 +2,12 @@ variable "region" {
   type    = string
   default = "ap-south-1"
 }
+variable "tagging" {
+  type = map(any)
+  default = {
+    #Name         = "SVC-ACCOUNT-${count.index}"
+    Environment  = "DEV"
+    CreationDate = "30/01/2023"
+    Project      = "TF-IAM-ACCOUNT"
+  }
+}
