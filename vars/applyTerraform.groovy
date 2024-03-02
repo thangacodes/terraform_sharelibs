e3a178056c1778caf03babef640965a13f060b23
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-03-03 00:27:50 +0530

    Update applyTerraform.groovy

--- vars/applyTerraform.groovy
+++ vars/applyTerraform.groovy
@@ -1,7 +1,7 @@
 def call(){
   echo "Terraform apply with auto approval in progress"
     sh '''
-      cd terraform_sharelibs/ec2_spinup
+      cd terraform_sharelibs/user_account
       terraform apply --auto-approve
   '''
 }
