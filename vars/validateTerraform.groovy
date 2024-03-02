71349dea8eff0a1be982ba5579e9a4a623b0f50f
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-03-03 00:29:08 +0530

    Update validateTerraform.groovy

--- vars/validateTerraform.groovy
+++ vars/validateTerraform.groovy
@@ -1,7 +1,7 @@
 def call(){
   echo "Terraform validate in progress.."
   sh '''
-      cd terraform_sharelibs/ec2_spinup
+      cd terraform_sharelibs/user_account
       terraform validate
   '''
 }
