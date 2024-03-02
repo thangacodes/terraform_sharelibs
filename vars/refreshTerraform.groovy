7b8fcd6707a0a0b76d239bbd70621b93ab31ef5d
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-03-03 00:48:00 +0530

    Update refreshTerraform.groovy

--- vars/refreshTerraform.groovy
+++ vars/refreshTerraform.groovy
@@ -1,7 +1,7 @@
 def call(){
   echo "Terraform state refresh will happen where the tf state file existing path.."
   sh '''
-     cd terraform_sharelibs/user_account
+     cd infraiac/user_account
      terraform refresh
      '''
 }
