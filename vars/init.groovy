5eeb3f5811dbdb94b0ca1d97a6d082ce4d4a4d4b
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-03-02 18:44:36 +0530

    Update init.groovy

--- vars/init.groovy
+++ vars/init.groovy
@@ -1,4 +1,9 @@
-def init(initialization){
-  echo "Terraform initialization in progress.."
-  sh 'terraform init'
+def init{
+                echo "Terraform initialization in progress"
+                sh '''
+                pwd
+                cd terraform_sharelibs/ec2_spinup
+                pwd
+                ls -lrt
+                '''
 }
