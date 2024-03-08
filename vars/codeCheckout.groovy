a57cce075afa692f9712d25eef63be7ce15b1813
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-03-08 20:22:54 +0530

    Update codeCheckout.groovy

--- vars/codeCheckout.groovy
+++ vars/codeCheckout.groovy
@@ -2,7 +2,6 @@ def call(){
 	echo "Cloning the code repo from GitHub.."
 	sh '''
               git clone https://github.com/thangacodes/terraform_sharelibs.git
-	      pwd
-              cd terraform_sharelibs/user_account
+              cd terraform_sharelibs/tfscripts/users_creation
 	      '''
 }
