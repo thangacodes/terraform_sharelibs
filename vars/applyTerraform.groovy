31d03d3b9c3c56ecd8d3a495dbd755538e4f7d6f
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-03-08 20:22:31 +0530

    Update applyTerraform.groovy

--- vars/applyTerraform.groovy
+++ vars/applyTerraform.groovy
@@ -1,7 +1,7 @@
 def call(){
   echo "Terraform apply with auto approval in progress"
     sh '''
-      cd terraform_sharelibs/user_account
+      cd terraform_sharelibs/tfscripts/users_creation
       terraform apply --auto-approve
   '''
 }
